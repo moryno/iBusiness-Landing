baf355b2c1344585049fd8a0ad6ad94347321c70
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2023-03-14 09:27:35 +0300

    Set user to local storage

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -4,5 +4,5 @@ export const logoutFunc = () => {
 
 export const setupLogin = (token, user) => {
   localStorage.setItem("token", token);
-  localStorage.setItem("user", user);
+  localStorage.setItem("user", JSON.stringify(user));
 };
